17d27fa8090eeeb6c5e77af224c657e431ae5898
data-luiz <luizfernandoaraujo.data@gmail.com>
2025-11-19 14:11:24 -0300

    construtores

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,58 +6,52 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
 
-        Product product = new Product();
+            System.out.println("STOCK OF PRODUCT");
+            System.out.print("Enter product name: ");
+            String name = sc.nextLine();
+            System.out.print("Enter product price: ");
+            double price = sc.nextDouble();
+            System.out.print("Enter product quantity in stock: ");
+            int quantity = sc.nextInt();
 
-        System.out.println("STOCK OF PRODUCT");
-        System.out.print("Enter product name: ");
-        product.name = sc.nextLine();
+            Product product = new Product(name, price, quantity);
 
-        System.out.print("Enter product price: ");
-        product.price = sc.nextDouble();
+            int option = 0;
 
-        System.out.print("Enter product quantity in stock: ");
-        product.quantity = sc.nextInt();
+            // Menu com IF
+            while (option != 4) {
+                System.out.println();
+                System.out.println("1 - Show product data");
+                System.out.println("2 - Add products to stock");
+                System.out.println("3 - Remove products from stock");
+                System.out.println("4 - Exit");
+                System.out.println();
+                System.out.print("Choose an option: ");
+                option = sc.nextInt();
 
-        int option = 0;
-
-        // Menu com IF
-        while (option != 4) {
-            System.out.println();
-            System.out.println("1 - Show product data");
-            System.out.println("2 - Add products to stock");
-            System.out.println("3 - Remove products from stock");
-            System.out.println("4 - Exit");
-            System.out.println();
-            System.out.print("Choose an option: ");
-            option = sc.nextInt();
-
-            if (option == 1) {
-                System.out.println("\nProduct data: " + product);
+                if (option == 1) {
+                    System.out.println("\nProduct data: " + product);
+                } else if (option == 2) {
+                    System.out.print("Enter quantity to add: ");
+                    int addQty = sc.nextInt();
+                    product.addProduct(addQty);
+                    System.out.println("Updated data: " + product);
+                } else if (option == 3) {
+                    System.out.print("Enter quantity to remove: ");
+                    int removeQty = sc.nextInt();
+                    product.removeProduct(removeQty);
+                    System.out.println("Updated data: " + product);
+                } else if (option == 4) {
+                    System.out.println("exiting the program");
+                } else {
+                    System.out.println("Invalid option. Try again!");
+                }
             }
-            else if (option == 2) {
-                System.out.print("Enter quantity to add: ");
-                int addQty = sc.nextInt();
-                product.addProduct(addQty);
-                System.out.println("Updated data: " + product);
-            }
-            else if (option == 3) {
-                System.out.print("Enter quantity to remove: ");
-                int removeQty = sc.nextInt();
-                product.removeProduct(removeQty);
-                System.out.println("Updated data: " + product);
-            }
-            else if (option == 4) {
-                System.out.println("exiting the program");
-            }
-            else {
-                System.out.println("Invalid option. Try again!");
-            }
+
+            sc.close();
         }
-
-        sc.close();
     }
 }
